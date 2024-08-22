1bdfb583f6157658cd1d7a9ac82162bace68e631
vallken <salvakennedy@gmail.com>
2024-08-22 14:49:26 +0700

    Change Method

--- plugin/igdl.js
+++ plugin/igdl.js
@@ -1,4 +1,31 @@
 const igdown = require("../helper/igDown");
+const axios = require("axios");
+
+async function getFileInfo(url) {
+  try {
+    const response = await axios.head(url);
+
+    const contentDisposition = response.headers["content-disposition"];
+    const contentType = response.headers["content-type"];
+
+    let fileName;
+    if (contentDisposition) {
+      const match = contentDisposition.match(/filename="(.+)"/);
+      if (match && match[1]) {
+        fileName = match[1];
+      }
+    }
+
+    if (!fileName) {
+      const ext = contentType ? `.${contentType.split("/")[1]}` : "";
+      fileName = `downloaded_file${ext}`;
+    }
+
+    return { fileName, contentType };
+  } catch (error) {
+    console.error("Failed to get file info:", error.message);
+  }
+}
 
 const execute = async (sock, msg, args) => {
   if (!args[0]) {
@@ -8,17 +35,26 @@ const execute = async (sock, msg, args) => {
   } else {
     try {
       const media = await igdown(args[0]);
-      media.data.url.map((media) => {
-        if (media.includes("jpg")) {
-          sock.sendMessage(msg.key.remoteJid, {
-            image: { url: media },
-          });
-        } else {
-          sock.sendMessage(msg.key.remoteJid, {
-            video: { url: media },
-          });
+      const processedUrls = new Set();
+
+      for (const item of media.data) {
+        if (!processedUrls.has(item.url)) {
+          const cekFile = await getFileInfo(item.url);
+
+          processedUrls.add(item.url);
+          // Uncomment this part when ready to send messages
+          if (cekFile.fileName.endsWith(".mp4")) {
+            await sock.sendMessage(msg.key.remoteJid, {
+              video: { url: item.url },
+              caption: cekFile.fileName,
+            });
+          } else {
+            await sock.sendMessage(msg.key.remoteJid, {
+              image: { url: item.url },
+            });
+          }
         }
-      });
+      }
     } catch (err) {
       console.error(err);
       sock.sendMessage(msg.key.remoteJid, {
